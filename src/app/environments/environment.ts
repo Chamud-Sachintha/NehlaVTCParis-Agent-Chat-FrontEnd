b6587091de6f9152facd2d17c90e697e576090d1
Chamud-Sachintha <chamudsachintha999@gmail.com>
2023-06-03 10:33:55 +0530

    add fix

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     app_uri: 'https://nehlavtcparis-agentchat-platform-production.up.railway.app/',
-    web_socket: 'http://nehlavtcparis-agentchat-platform-production.up.railway.app/ws'
+    web_socket: 'https://nehlavtcparis-agentchat-platform-production.up.railway.app/ws'
 };
